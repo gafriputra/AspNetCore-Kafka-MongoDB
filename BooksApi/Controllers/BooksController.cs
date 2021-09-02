ee9c11a62181d6b26a54961dfe355775c7992190
Gafri.Putra <Gafri.Putra@SYSTEM-08241>
2021-09-02 16:45:44 +0700

    Add project files.

---
+++ BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,107 @@
+﻿using BooksApi.Models;
+using BooksApi.Services;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> Get() =>
+            _bookService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<Book> Get(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Create(Book book)
+        {
+            book.Id = null;
+            _bookService.Create(book);
+
+            return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public ActionResult<Book> Update(string id, Book bookIn)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Update(id, bookIn);
+            return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, bookIn);
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Remove(book.Id);
+
+            return NoContent();
+        }
+
+        [HttpPost("push-kafka")]
+        public ActionResult<Book> PushKafka(Book book)
+        {
+            var conf = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            Action<DeliveryReport<Null, string>> handler = r =>
+                Console.WriteLine(!r.Error.IsError
+                    ? $"Delivered message to {r.TopicPartitionOffset}"
+                    : $"Delivery Error: {r.Error.Reason}");
+
+            using (var p = new ProducerBuilder<Null, string>(conf).Build())
+            {
+                string jsonString = JsonSerializer.Serialize(book);
+                p.Produce("quickstart-events", new Message<Null, string> { Value = jsonString }, handler);
+                Console.WriteLine(jsonString + "quickstart-events");
+                // wait for up to 10 seconds for any inflight messages to be delivered.
+                p.Flush(TimeSpan.FromSeconds(10));
+            }
+            return book;
+        }
+
+        [HttpPost("push-mongo")]
+        public ActionResult<Book> PushMongo(Book book)
+        {
+            var data = _bookService.GetBookName(book.BookName);
+            return data == null ? Create(book) : Update(data.Id, book);
+        }
+    }
+}
